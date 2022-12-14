1594d0e2448e9b14734514d05afbf4a4cbeafe6d
AlyonaUkr <mochulska1994@gmail.com>
2022-12-14 16:37:16 +0200

    ready

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const firstRow = prompt('Write your name, please');
+const secondRow = prompt('Write color of your eyes, please');
+const thirdRow = prompt('Write your favorite movie, please');
+
+const userResults = '' + firstRow + ', ' + secondRow + ', ' + thirdRow + '';
+alert(userResults);
+
+const fiveNumber = '3' + ' ' + '5' + ' ' + '6' + ' ' + '2' + ' ' + "1";
+console.log(fiveNumber);
