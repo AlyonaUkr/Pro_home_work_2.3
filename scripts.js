ec9b39a0c8dc24aead0292c2d4ef48771411c541
AlyonaUkr <mochulska1994@gmail.com>
2022-12-14 21:37:28 +0200

    the end

--- scripts.js
+++ scripts.js
@@ -5,5 +5,5 @@ const thirdRow = prompt('Write your favorite movie, please');
 const userResults = '' + firstRow + ', ' + secondRow + ', ' + thirdRow + '';
 alert(userResults);
 
-const fiveNumber = '3' + ' ' + '5' + ' ' + '6' + ' ' + '2' + ' ' + "1";
-console.log(fiveNumber);
+const fiveNumber = '35621';
+console.log(fiveNumber[0], fiveNumber[1], fiveNumber[2], fiveNumber[3], fiveNumber[4]);
